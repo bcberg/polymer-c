7aa46f25ac72998be65735002c5f7a4feaa1817e
katiebogue <katharinebogue@gmail.com>
2022-05-11 15:56:51 -0700

    HPC files added

---
+++ drivers/Submitbash_double.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#./metropolis.out parameters.txt outputfile verboseTF NFil N iSite baseSepDist Force dimerForce
+
+d=`date +%Y.%d.%m`
+
+
+#Edit these parameters:
+for NumSeg in $(seq 1 200)
+do
+what='double' #'single' 'double'
+NFil=2 #2
+#NumSeg=${i} #single=300; double=200; dimer=122
+if [ ${NFil} -eq 2 ]
+then
+	baseSepDist=16.667000
+else
+  baseSepDist=0
+fi;
+dimerForce=0 #10
+iSite='-1'
+force=0
+#
+
+
+#echo submit.${what}.N${NumSeg}.${d}.sh
+
+cp testsubmit.sh submit.${what}.N${NumSeg}.${d}.sh
+sed -i "34c\ ./metropolis.out parameters.txt output_${what}.N${NumSeg}.iSite${iSite}.BSD${baseSepDist}.force${force}.kdimer${dimerForce}.txt 0 ${NFil} ${NumSeg} ${iSite} ${baseSepDist} ${force} ${dimerForce}
+" "submit.${what}.N${NumSeg}.${d}.sh"
+done
+
+
+cd /u/home/k/katiebog/Data/
+mkdir double.${d}
+
+for NumSeg in $(seq 1 200)
+do
+cd /u/home/k/katiebog/Data/double.${d}
+mkdir run.${what}.N${NumSeg}_${d}
+cd /u/home/k/katiebog/test/src/PolymerCode
+cp metropolis.out parameters.txt ISEED submit.${what}.N${NumSeg}.${d}.sh /u/home/k/katiebog/Data/double.${d}/run.${what}.N${NumSeg}_${d}
+cd /u/home/k/katiebog/test/src/PolymerCode
+rm submit.${what}.N${NumSeg}.${d}.sh
+done
+
+for NumSeg in $(seq 1 200)
+do
+cd /u/home/k/katiebog/Data/double.${d}/run.${what}.N${NumSeg}_${d}
+qsub ./submit.${what}.N${NumSeg}.${d}.sh
+done
---
+++ drivers/Submitbash_single.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#./metropolis.out parameters.txt outputfile verboseTF NFil N iSite baseSepDist Force dimerForce
+
+d=`date +%Y.%d.%m`
+
+
+#Edit these parameters:
+for NumSeg in $(seq 1 300)
+do
+what='single' #'single' 'double'
+NFil=1 #2
+#NumSeg=${i} #single=300; double=200; dimer=122
+if [ ${NFil} -eq 2 ]
+then
+	baseSepDist=16.667000
+else
+  baseSepDist=0
+fi;
+dimerForce=0 #10
+iSite='-1'
+force=0
+#
+
+
+
+#echo submit.${what}.N${NumSeg}.${d}.sh
+
+cp testsubmit.sh submit.${what}.N${NumSeg}.${d}.sh
+sed -i "34c\ ./metropolis.out parameters.txt output_${what}.N${NumSeg}.iSite${iSite}.BSD${baseSepDist}.force${force}.kdimer${dimerForce}.txt 0 ${NFil} ${NumSeg} ${iSite} ${baseSepDist} ${force} ${dimerForce}
+" "submit.${what}.N${NumSeg}.${d}.sh"
+done
+
+cd /u/home/k/katiebog/Data/
+mkdir single.${d}
+
+for NumSeg in $(seq 1 300)
+do
+cd /u/home/k/katiebog/Data/single.${d}
+mkdir run.${what}.N${NumSeg}_${d}
+cd /u/home/k/katiebog/test/src/PolymerCode
+cp metropolis.out parameters.txt ISEED submit.${what}.N${NumSeg}.${d}.sh /u/home/k/katiebog/Data/single.${d}/run.${what}.N${NumSeg}_${d}
+cd /u/home/k/katiebog/test/src/PolymerCode
+rm submit.${what}.N${NumSeg}.${d}.sh
+done
+
+for NumSeg in $(seq 1 300)
+do
+cd /u/home/k/katiebog/Data/single.${d}/run.${what}.N${NumSeg}_${d}
+qsub ./submit.${what}.N${NumSeg}.${d}.sh
+done
