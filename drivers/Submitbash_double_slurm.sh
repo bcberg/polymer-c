dbce26debd7158c4e49c2466f2ed7ec6e7a8da21
katiebogue <katharinebogue@gmail.com>
2023-02-17 16:11:25 -0800

    Create Submitbash_double_slurm.sh

---
+++ drivers/Submitbash_double_slurm.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#./metropolis.out parameters.txt outputfile verboseTF NFil N iSite baseSepDist Force dimerForce
+
+#d=`date +%Y.%d.%m`
+#d='2023.15.01'
+d=$(date +%Y.%d.%m.%T)
+
+# polymer number of segments to sweep over
+NStart=350
+NStop=350
+
+# output directory
+output_dir=/pub/kbogue1/GitHub/Data/polymer-c_data/
+
+# dimerization state
+what='double' #'single' 'double'
+
+#Edit these parameters:
+for NumSeg in $(seq $NStart $NStop); do
+  NFil=2        #2
+  #NumSeg=${i} #single=300; double=200; dimer=122
+  if [ ${NFil} -eq 2 ]; then
+    baseSepDist=16.667000
+  else
+    baseSepDist=0
+  fi
+  dimerForce=0 #10
+  iSite='-1'
+  force=0
+  #
+
+  #echo submit.${what}.N${NumSeg}.${d}.sh
+
+  cp testslurm.sub submit.${what}.N${NumSeg}.${d}.sub
+  sed -i "23c\ ./metropolis.out parameters.txt output_${what}.N${NumSeg}.iSite${iSite}.BSD${baseSepDist}.force${force}.kdimer${dimerForce}.txt 0 ${NFil} ${NumSeg} ${iSite} ${baseSepDist} ${force} ${dimerForce}
+" "submit.${what}.N${NumSeg}.${d}.sub"
+  sed -i "3c\#SBATCH --job-name=${what}_${NumSeg}      ## Name of the job.
+" "submit.${what}.N${NumSeg}.${d}.sub"
+done
+
+mkdir $output_dir/${what}.${d}
+
+for NumSeg in $(seq $NStart $NStop); do
+  mkdir $output_dir/${what}.${d}/run.${what}.N${NumSeg}_${d}
+  cp ../src/PolymerCode/metropolis.out ../src/PolymerCode/parameters.txt ../src/PolymerCode/ISEED $output_dir/${what}.${d}/run.${what}.N${NumSeg}_${d}
+  cp submit.${what}.N${NumSeg}.${d}.sub $output_dir/${what}.${d}/run.${what}.N${NumSeg}_${d}
+  rm submit.${what}.N${NumSeg}.${d}.sub
+done
+
+for NumSeg in $(seq $NStart $NStop); do
+  cd $output_dir/${what}.${d}/run.${what}.N${NumSeg}_${d}
+  sbatch submit.${what}.N${NumSeg}.${d}.sub
+done
